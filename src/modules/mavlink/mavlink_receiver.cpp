santiagomf98/Firmware

mavlink: send LINK_NODE_STATUS
 - receiver estimate total lost messages

src/modules/mavlink/mavlink_receiver.cpp
@@ -2946,6 +2946,7 @@ MavlinkReceiver::Run()
 				/* if read failed, this loop won't execute */
 				for (ssize_t i = 0; i < nread; i++) {
 					if (mavlink_parse_char(_mavlink->get_channel(), buf[i], &msg, &_status)) {
+						_total_received_counter++;
 
 						/* check if we received version 2 and request a switch. */
 						if (!(_mavlink->get_status()->flags & MAVLINK_STATUS_FLAG_IN_MAVLINK1)) {
@@ -2976,12 +2977,111 @@ MavlinkReceiver::Run()
 
 						/* handle packet with parent object */
 						_mavlink->handle_message(&msg);
+
+
+						// calculate lost messages for this system id
+						bool px4_sysid_index_found = false;
+						int px4_sysid_index = 0;
+
+						if (msg.sysid != mavlink_system.sysid) {
+							for (int sys_id = 1; sys_id < MAX_REMOTE_SYSTEM_IDS; sys_id++) {
+								if (_system_id_map[sys_id] == 0) {
+									// slot available
+									px4_sysid_index_found = true;
+									px4_sysid_index = sys_id;
+									break;
+								}
+							}
+
+							PX4_ERR("not enough system id slots");
+
+						} else {
+							px4_sysid_index_found = true;
+						}
+
+						// find PX4 component id
+						uint8_t px4_comp_id = 0;
+						bool px4_comp_id_found = false;
+
+						for (int id = 0; id < COMP_ID_MAX; id++) {
+							if (supported_component_map[id] == msg.compid) {
+								px4_comp_id = id;
+								px4_comp_id_found = true;
+								break;
+							}
+						}
+
+						if (!px4_comp_id_found) {
+							PX4_WARN("unsupported component id, msgid: %d, sysid: %d compid: %d", msg.msgid, msg.sysid, msg.compid);
+						}
+
+						if (px4_comp_id_found && px4_sysid_index_found) {
+							// Increase receive counter
+							_total_received_supported_counter++;
+
+							uint8_t last_seq = _last_index[px4_sysid_index][px4_comp_id];
+							uint8_t expected_seq = last_seq + 1;
+
+							// Determine what the next expected sequence number is, accounting for
+							// never having seen a message for this system/component pair.
+							if (_first_message[px4_comp_id]) {
+								_first_message[px4_sysid_index][px4_comp_id] = 0;
+								last_seq = msg.seq;
+								expected_seq = msg.seq;
+							}
+
+							// And if we didn't encounter that sequence number, record the error
+							if (msg.seq != expected_seq) {
+								int lost_messages = 0;
+
+								// Account for overflow during packet loss
+								if (msg.seq < expected_seq) {
+									lost_messages = (msg.seq + 255) - expected_seq;
+
+								} else {
+									lost_messages = msg.seq - expected_seq;
+								}
+
+								// Log how many were lost
+								_total_lost_counter += lost_messages;
+							}
+
+							// And update the last sequence number for this system/component pair
+							_last_index[px4_sysid_index][px4_comp_id] = msg.seq;
+
+							// Calculate new loss ratio
+							const float total_sent = _total_received_supported_counter + _total_lost_counter;
+							float receiveLossPercent = (_total_lost_counter / total_sent) * 100.f;
+
+							_running_loss_percent = (receiveLossPercent * 0.5f) + (_running_loss_percent * 0.5f);
+						}
 					}
 				}
 
 				/* count received bytes (nread will be -1 on read error) */
 				if (nread > 0) {
 					_mavlink->count_rxbytes(nread);
+
+					telemetry_status_s &tstatus = _mavlink->get_telemetry_status();
+					tstatus.rx_message_count = _total_received_counter;
+					tstatus.rx_message_supported_count = _total_received_supported_counter;
+					tstatus.rx_message_lost_count = _total_lost_counter;
+					tstatus.rx_message_lost_rate = _running_loss_percent;
+
+					if (_mavlink_status_last_buffer_overrun != _status.buffer_overrun) {
+						tstatus.rx_buffer_overruns++;
+						_mavlink_status_last_buffer_overrun = _status.buffer_overrun;
+					}
+
+					if (_mavlink_status_last_parse_error != _status.parse_error) {
+						tstatus.rx_parse_errors++;
+						_mavlink_status_last_parse_error = _status.parse_error;
+					}
+
+					if (_mavlink_status_last_packet_rx_drop_count != _status.packet_rx_drop_count) {
+						tstatus.rx_packet_drop_count++;
+						_mavlink_status_last_packet_rx_drop_count = _status.packet_rx_drop_count;
+					}
 				}
 
 #if defined(MAVLINK_UDP)

src/modules/mavlink/streams/LINK_NODE_STATUS.hpp
@@ -1,0 +1,80 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef LINK_NODE_STATUS_HPP
+#define LINK_NODE_STATUS_HPP
+
+class MavlinkStreamLinkNodeStatus : public MavlinkStream
+{
+public:
+	static MavlinkStream *new_instance(Mavlink *mavlink) { return new MavlinkStreamLinkNodeStatus(mavlink); }
+
+	static constexpr const char *get_name_static() { return "LINK_NODE_STATUS"; }
+	static constexpr uint16_t get_id_static() { return MAVLINK_MSG_ID_LINK_NODE_STATUS; }
+
+	const char *get_name() const override { return get_name_static(); }
+	uint16_t get_id() override { return get_id_static(); }
+
+	unsigned get_size() override
+	{
+		return MAVLINK_MSG_ID_LINK_NODE_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
+	}
+
+private:
+	explicit MavlinkStreamLinkNodeStatus(Mavlink *mavlink) : MavlinkStream(mavlink) {}
+
+	bool send() override
+	{
+		mavlink_link_node_status_t link_node_status{};
+
+		const telemetry_status_s &tstatus = _mavlink->get_telemetry_status();
+		link_node_status.tx_buf = 0; // % TODO
+		link_node_status.rx_buf = 0; // % TODO
+		link_node_status.tx_rate = tstatus.tx_rate_avg;
+		link_node_status.rx_rate = tstatus.rx_rate_avg;
+		link_node_status.rx_parse_err = tstatus.rx_parse_errors;
+		link_node_status.tx_overflows = tstatus.tx_buffer_overruns;
+		link_node_status.rx_overflows = tstatus.rx_buffer_overruns;
+		link_node_status.messages_sent = tstatus.tx_message_count;
+		link_node_status.messages_received = tstatus.rx_message_count;
+		link_node_status.messages_lost = tstatus.rx_message_lost_count;
+
+		link_node_status.timestamp = hrt_absolute_time();
+
+		mavlink_msg_link_node_status_send_struct(_mavlink->get_channel(), &link_node_status);
+
+		return true;
+	}
+};
+
+#endif // LINK_NODE_STATUS_HPP

rpstanley90/PX4-Autopilot

src/modules/mavlink/mavlink_receiver.cpp
@@ -3029,6 +3029,7 @@ MavlinkReceiver::Run()
 				/* if read failed, this loop won't execute */
 				for (ssize_t i = 0; i < nread; i++) {
 					if (mavlink_parse_char(_mavlink->get_channel(), buf[i], &msg, &_status)) {
+						_total_received_counter++;
 
 						/* check if we received version 2 and request a switch. */
 						if (!(_mavlink->get_status()->flags & MAVLINK_STATUS_FLAG_IN_MAVLINK1)) {
@@ -3059,12 +3060,111 @@ MavlinkReceiver::Run()
 
 						/* handle packet with parent object */
 						_mavlink->handle_message(&msg);
+
+
+						// calculate lost messages for this system id
+						bool px4_sysid_index_found = false;
+						int px4_sysid_index = 0;
+
+						if (msg.sysid != mavlink_system.sysid) {
+							for (int sys_id = 1; sys_id < MAX_REMOTE_SYSTEM_IDS; sys_id++) {
+								if (_system_id_map[sys_id] == 0) {
+									// slot available
+									px4_sysid_index_found = true;
+									px4_sysid_index = sys_id;
+									break;
+								}
+							}
+
+							PX4_ERR("not enough system id slots");
+
+						} else {
+							px4_sysid_index_found = true;
+						}
+
+						// find PX4 component id
+						uint8_t px4_comp_id = 0;
+						bool px4_comp_id_found = false;
+
+						for (int id = 0; id < COMP_ID_MAX; id++) {
+							if (supported_component_map[id] == msg.compid) {
+								px4_comp_id = id;
+								px4_comp_id_found = true;
+								break;
+							}
+						}
+
+						if (!px4_comp_id_found) {
+							PX4_WARN("unsupported component id, msgid: %d, sysid: %d compid: %d", msg.msgid, msg.sysid, msg.compid);
+						}
+
+						if (px4_comp_id_found && px4_sysid_index_found) {
+							// Increase receive counter
+							_total_received_supported_counter++;
+
+							uint8_t last_seq = _last_index[px4_sysid_index][px4_comp_id];
+							uint8_t expected_seq = last_seq + 1;
+
+							// Determine what the next expected sequence number is, accounting for
+							// never having seen a message for this system/component pair.
+							if (_first_message[px4_comp_id]) {
+								_first_message[px4_sysid_index][px4_comp_id] = 0;
+								last_seq = msg.seq;
+								expected_seq = msg.seq;
+							}
+
+							// And if we didn't encounter that sequence number, record the error
+							if (msg.seq != expected_seq) {
+								int lost_messages = 0;
+
+								// Account for overflow during packet loss
+								if (msg.seq < expected_seq) {
+									lost_messages = (msg.seq + 255) - expected_seq;
+
+								} else {
+									lost_messages = msg.seq - expected_seq;
+								}
+
+								// Log how many were lost
+								_total_lost_counter += lost_messages;
+							}
+
+							// And update the last sequence number for this system/component pair
+							_last_index[px4_sysid_index][px4_comp_id] = msg.seq;
+
+							// Calculate new loss ratio
+							const float total_sent = _total_received_supported_counter + _total_lost_counter;
+							float receiveLossPercent = (_total_lost_counter / total_sent) * 100.f;
+
+							_running_loss_percent = (receiveLossPercent * 0.5f) + (_running_loss_percent * 0.5f);
+						}
 					}
 				}
 
 				/* count received bytes (nread will be -1 on read error) */
 				if (nread > 0) {
 					_mavlink->count_rxbytes(nread);
+
+					telemetry_status_s &tstatus = _mavlink->telemetry_status();
+					tstatus.rx_message_count = _total_received_counter;
+					tstatus.rx_message_supported_count = _total_received_supported_counter;
+					tstatus.rx_message_lost_count = _total_lost_counter;
+					tstatus.rx_message_lost_rate = _running_loss_percent;
+
+					if (_mavlink_status_last_buffer_overrun != _status.buffer_overrun) {
+						tstatus.rx_buffer_overruns++;
+						_mavlink_status_last_buffer_overrun = _status.buffer_overrun;
+					}
+
+					if (_mavlink_status_last_parse_error != _status.parse_error) {
+						tstatus.rx_parse_errors++;
+						_mavlink_status_last_parse_error = _status.parse_error;
+					}
+
+					if (_mavlink_status_last_packet_rx_drop_count != _status.packet_rx_drop_count) {
+						tstatus.rx_packet_drop_count++;
+						_mavlink_status_last_packet_rx_drop_count = _status.packet_rx_drop_count;
+					}
 				}
 
 #if defined(MAVLINK_UDP)

src/modules/mavlink/streams/LINK_NODE_STATUS.hpp
@@ -1,0 +1,84 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef LINK_NODE_STATUS_HPP
+#define LINK_NODE_STATUS_HPP
+
+class MavlinkStreamLinkNodeStatus : public MavlinkStream
+{
+public:
+	static MavlinkStream *new_instance(Mavlink *mavlink) { return new MavlinkStreamLinkNodeStatus(mavlink); }
+
+	static constexpr const char *get_name_static() { return "LINK_NODE_STATUS"; }
+	static constexpr uint16_t get_id_static() { return MAVLINK_MSG_ID_LINK_NODE_STATUS; }
+
+	const char *get_name() const override { return get_name_static(); }
+	uint16_t get_id() override { return get_id_static(); }
+
+	unsigned get_size() override
+	{
+		return MAVLINK_MSG_ID_LINK_NODE_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
+	}
+
+private:
+	explicit MavlinkStreamLinkNodeStatus(Mavlink *mavlink) : MavlinkStream(mavlink) {}
+
+	bool send() override
+	{
+		if (_mavlink->get_free_tx_buf() >= get_size()) {
+			mavlink_link_node_status_t link_node_status{};
+
+			const telemetry_status_s &tstatus = _mavlink->telemetry_status();
+			link_node_status.tx_buf = 0; // % TODO
+			link_node_status.rx_buf = 0; // % TODO
+			link_node_status.tx_rate = tstatus.tx_rate_avg;
+			link_node_status.rx_rate = tstatus.rx_rate_avg;
+			link_node_status.rx_parse_err = tstatus.rx_parse_errors;
+			link_node_status.tx_overflows = tstatus.tx_buffer_overruns;
+			link_node_status.rx_overflows = tstatus.rx_buffer_overruns;
+			link_node_status.messages_sent = tstatus.tx_message_count;
+			link_node_status.messages_received = tstatus.rx_message_count;
+			link_node_status.messages_lost = tstatus.rx_message_lost_count;
+
+			link_node_status.timestamp = hrt_absolute_time();
+
+			mavlink_msg_link_node_status_send_struct(_mavlink->get_channel(), &link_node_status);
+
+			return true;
+		}
+
+		return false;
+	}
+};
+
+#endif // LINK_NODE_STATUS_HPP